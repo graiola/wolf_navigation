a6c1b70232bbfd11d2ebc22a906ee9c624090cf4
Federico Rollo <rollo.f96@gmail.com>
2022-02-07 17:07:56 +0100

    added ros parameter for odom publisher node

--- src/odom_publisher_node.cpp
+++ src/odom_publisher_node.cpp
@@ -4,13 +4,14 @@
 
 static geometry_msgs::TransformStamped _odom_trans;
 static ros::Time _prev_stamp{0.0};
+static std::string _odom_topic{""}, _odom_topic_child{""};
 
 void convertOdom(const nav_msgs::Odometry::ConstPtr& odom)
 {
   //first, we'll publish the transform over tf
   _odom_trans.header.stamp = odom->header.stamp;
-  _odom_trans.header.frame_id = "odom";
-  _odom_trans.child_frame_id = "base_footprint";
+  _odom_trans.header.frame_id = _odom_topic;
+  _odom_trans.child_frame_id = _odom_topic_child;
 
   _odom_trans.transform.translation.x = odom->pose.pose.position.x;
   _odom_trans.transform.translation.y = odom->pose.pose.position.y;
@@ -20,24 +21,39 @@ void convertOdom(const nav_msgs::Odometry::ConstPtr& odom)
 
 
 int main(int argc, char** argv){
-  ros::init(argc, argv, "odometry_publisher");
+  std::string ns{"odometry_publisher"};
 
-  ros::NodeHandle n;
-  ros::Subscriber odom_sub = n.subscribe("t265/odom", 20, convertOdom);
+  ros::init(argc, argv, ns);
+
+  ros::NodeHandle n(ns); // load the relative namespace
+
+//  set odom subscriber topic
+  std::string odom_sub_topic;
+  n.getParam("odom_sub_topic", odom_sub_topic);
+
+//  set global odom topics
+  n.getParam("odom_topic", _odom_topic);
+  n.getParam("odom_topic_child", _odom_topic_child);
+
+//  initialize odom subscriber subscriber
+  ros::Subscriber odom_sub = n.subscribe(odom_sub_topic, 20, convertOdom);
+//  initialize tf odom broadcaster
   tf::TransformBroadcaster odom_broadcaster;
 
+//   set ros rate
+  double ros_rate;
+  n.getParam("ros_rate", ros_rate);
+  ros::Rate r(ros_rate);
 
-
-  ros::Rate r(10.0);
   while(n.ok()){
 
     ros::spinOnce();
-
+//    check if the current read tf has not been already sent
     if (_prev_stamp != _odom_trans.header.stamp)
     {
-      //send the transform
+//       send the transform
       odom_broadcaster.sendTransform(_odom_trans);
-
+//      update the previous odom time
       _prev_stamp = _odom_trans.header.stamp;
     }
 

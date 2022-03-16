b03362c8b402655c769c1d632726bfa514839e03
Gennaro Raiola <gennaro.raiola@gmail.com>
2022-03-16 18:28:07 +0100

    Working on it

--- src/odom_publisher_node.cpp
+++ src/odom_publisher_node.cpp
@@ -2,63 +2,81 @@
 #include <tf2/transform_datatypes.h>
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_listener.h>
+#include <tf2_ros/transform_broadcaster.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <nav_msgs/Odometry.h>
-
-
 
 static std::string _tracking_camera_frame_id;
 static std::string _base_footprint_frame_id;
 static std::string _odom_frame_id;
-static tf2::Stamped<tf2::Transform> _tracking_camera_T_basefoot_print;
-static tf2::Stamped<tf2::Transform> _odom_T_tracking_camera;
-static tf2::Stamped<tf2::Transform> _odom_T_basefoot_print;
-static tf2_ros::Buffer _tf_buffer;
-static tf2_ros::TransformListener _tf_listener(_tf_buffer);
+
+struct RosTransformHandler {
+
+    typedef std::shared_ptr<RosTransformHandler> Ptr;
+
+    RosTransformHandler(): _tf_listener(_tf_buffer) {}
+
+    tf2::Stamped<tf2::Transform> _tracking_camera_T_basefoot_print;
+    tf2::Stamped<tf2::Transform> _odom_T_tracking_camera;
+    tf2::Stamped<tf2::Transform> _odom_T_basefoot_print;
+    tf2_ros::Buffer _tf_buffer;
+    tf2_ros::TransformListener _tf_listener;
+    tf2_ros::TransformBroadcaster _br;
+};
+
+RosTransformHandler::Ptr _data;
 
 // The tracking camera provides us the following transform odom_T_tracking_camera
 // we need to create: odom_T_basefoot_print = odom_T_tracking_camera * tracking_camera_T_basefoot_print
-
 void callback(const nav_msgs::Odometry::ConstPtr& odom_T_tracking_camera)
 {
 
-  //_odom_T_basefoot_print.header.stamp    = ros::Time::now();
-  //_odom_T_basefoot_print.header.frame_id = _odom_frame_id;
-  //_odom_T_basefoot_print.child_frame_id  = _base_footprint_frame_id;
-
   try
   {
-    _tracking_camera_T_basefoot_print. = _tf_buffer.lookupTransform(_base_footprint_frame_id,_tracking_camera_frame_id,ros::Time(0));
+    //auto T = _data->_tf_buffer.lookupTransform(_base_footprint_frame_id,_tracking_camera_frame_id,ros::Time(0));
+    //_data->_tracking_camera_T_basefoot_print.setOrigin(tf2::Vector3(T.transform.translation.x,T.transform.translation.y,T.transform.translation.z));
+    //_data->_tracking_camera_T_basefoot_print.setRotation(tf2::Quaternion(T.transform.rotation.x,T.transform.rotation.y,T.transform.rotation.z,T.transform.rotation.w));
+    tf2::fromMsg(_data->_tf_buffer.lookupTransform(_base_footprint_frame_id,_tracking_camera_frame_id,ros::Time(0)),_data->_tracking_camera_T_basefoot_print);
+
+    //_data->_tracking_camera_T_basefoot_print.setOrigin(tf2::Vector3(_data->_tracking_camera_T_basefoot_print.getOrigin().getX(),
+    //                                                                _data->_tracking_camera_T_basefoot_print.getOrigin().getY(),
+    //                                                                _data->_tracking_camera_T_basefoot_print.getOrigin().getX());
   }
   catch (tf2::TransformException &ex)
   {
     ROS_WARN("%s",ex.what());
   }
 
-  _odom_T_tracking_camera.transform.translation.x = odom_T_tracking_camera->pose.pose.position.x;
-  _odom_T_tracking_camera.transform.translation.y = odom_T_tracking_camera->pose.pose.position.y;
-  _odom_T_tracking_camera.transform.translation.z = odom_T_tracking_camera->pose.pose.position.z;
-
-  _odom_T_tracking_camera.transform.rotation.x = odom_T_tracking_camera->pose.pose.orientation.x;
-  _odom_T_tracking_camera.transform.rotation.y = odom_T_tracking_camera->pose.pose.orientation.y;
-  _odom_T_tracking_camera.transform.rotation.z = odom_T_tracking_camera->pose.pose.orientation.z;
-  _odom_T_tracking_camera.transform.rotation.w = odom_T_tracking_camera->pose.pose.orientation.w;
-
-
+  _data->_odom_T_tracking_camera.setOrigin(tf2::Vector3(odom_T_tracking_camera->pose.pose.position.x,odom_T_tracking_camera->pose.pose.position.y,odom_T_tracking_camera->pose.pose.position.z));
+  _data->_odom_T_tracking_camera.setRotation(tf2::Quaternion(odom_T_tracking_camera->pose.pose.orientation.x,odom_T_tracking_camera->pose.pose.orientation.y,odom_T_tracking_camera->pose.pose.orientation.z,odom_T_tracking_camera->pose.pose.orientation.w));
 
   //_odom_T_basefoot_print.transform.translation.x = odom->pose.pose.position.x;
   //_odom_T_basefoot_print.transform.translation.y = odom->pose.pose.position.y;
   //_odom_T_basefoot_print.transform.translation.z = 0; // not set to odom->pose.pose.position.z because during stand up the robot fly
   //_odom_T_basefoot_print.transform.rotation = odom->pose.pose.orientation;
 
+  _data->_odom_T_basefoot_print.mult(_data->_odom_T_tracking_camera,_data->_tracking_camera_T_basefoot_print);
 
-  _odom_T_basefoot_print.mult(_odom_T_tracking_camera,_tracking_camera_T_basefoot_print);
+  //geometry_msgs::TransformStamped T;
+  //T.transform  = tf2::toMsg(_data->_odom_T_basefoot_print.inverse());
+  //T.header.seq++;
+  //T.header.stamp = ros::Time::now();
+  //T.header.frame_id = _base_footprint_frame_id;
+  //T.child_frame_id = _odom_frame_id;
+
+  auto T = tf2::toMsg(_data->_odom_T_basefoot_print);
+  T.header.seq++;
+  T.header.stamp = ros::Time::now();
+  T.header.frame_id = _odom_frame_id;
+  T.child_frame_id = _base_footprint_frame_id;
+
+  _data->_br.sendTransform(T);
 }
 
 
 int main(int argc, char** argv)
 {
-  std::string ns{"odometry_publisher"};
+  std::string ns = "odometry_publisher";
 
   ros::init(argc, argv, ns);
 
@@ -70,6 +88,9 @@ int main(int argc, char** argv)
   n.getParam("output_topic", output_topic);
   n.getParam("tracking_camera_frame_id", _tracking_camera_frame_id);
   n.getParam("base_footprint_frame_id", _base_footprint_frame_id);
+  n.getParam("odom_frame_id", _odom_frame_id);
+
+  _data = std::make_shared<RosTransformHandler>();
 
   // initialize odom subscriber
   ros::Subscriber tracking_camera_sub = n.subscribe(tracking_camera_topic, 20, callback);
